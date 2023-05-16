e0c33248fb8f0755d7868a8a1c22c1ec44e1e507
massimo <donimassimo3@gmail.com>
2023-05-16 12:42:06 +0200

    a

--- flutter_django_api/api_flutter/urls.py
+++ flutter_django_api/api_flutter/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+#prova
+# cazzo
 urlpatterns = [
     path('name', view=views.hello_world),
     path('login', view=views.login),
