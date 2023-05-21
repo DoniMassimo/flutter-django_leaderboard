cd591aa857d4a5f567dfc0b0972f81ae0f998624
massimo <donimassimo3@gmail.com>
2023-05-21 11:52:25 +0200

    futter serve by django

--- pyenv/flutter_django_api/flutter_django_api/urls.py
+++ pyenv/flutter_django_api/flutter_django_api/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FLUTTER_WEB_APP = os.path.join(BASE_DIR, 'flutter_web_app')
+
+
+def flutter_redirect(request, resource):
+    return serve(request, resource, FLUTTER_WEB_APP)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_flutter.urls')),
+    path('flutter_web_app/', lambda r: flutter_redirect(r, 'index.html')),
+    path('flutter_web_app/<path:resource>', flutter_redirect),
 ]
